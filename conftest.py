786d64690b2a454a04a5337dbc6e81daff6f511f
Антон Лукин <shahter86@mail.ru>
2024-11-02 12:40:30 +0300

    На ревью 2.0

--- conftest.py
+++ conftest.py
@@ -1,4 +1,4 @@
-'''Вспомогательные инструменты для тестов web-сервиса «Stellar Burgers».'''
+'''Фикстуры для тестов web-сервиса «Stellar Burgers».'''
 import random
 import string
 
@@ -7,61 +7,8 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
+from data import EMAIL, MAIN_PAGE, PASSWORD
 from tests.locators import Locators as loc
-
-
-@pytest.fixture(scope='session')
-def user_name():
-    '''Имя пользователя существующего аккаунта.'''
-    return 'Антон'
-
-
-@pytest.fixture(scope='session')
-def email():
-    '''Логин существующего аккаунта.'''
-    return 'antonsprint5@mail.ru'
-
-
-@pytest.fixture(scope='session')
-def password():
-    '''Пароль существующего аккаунта.'''
-    return "You'llNeverGuess"
-
-
-@pytest.fixture(scope='class')
-def active():
-    '''Маркер активной категории конструктора.'''
-    return 'current'
-
-
-@pytest.fixture(scope='class')
-def constructor_header():
-    '''Заголовок блока конструктора.'''
-    return 'Соберите бургер'
-
-
-@pytest.fixture(scope='class')
-def personal_account_text():
-    '''Текст в личном кабинете.'''
-    return 'В этом разделе вы можете изменить свои персональные данные'
-
-
-@pytest.fixture(scope='class')
-def sign_in_form_title():
-    '''Заголовок формы авторизации.'''
-    return 'Вход'
-
-
-@pytest.fixture(scope='class')
-def invalid_password():
-    '''Недопустимый пароль.'''
-    return '1'
-
-
-@pytest.fixture(scope='class')
-def warning():
-    '''Предупреждение о недопустимом пароле.'''
-    return 'Некорректный пароль'
 
 
 @pytest.fixture(scope='function')
@@ -101,20 +48,21 @@ def generate_login():
 @pytest.fixture(scope='function')
 def driver():
     '''Создаёт драйвер браузера Google Chrome, открывает домашнюю страницу
-    web-сервиса «Stellar Burgers».'''
+    web-сервиса «Stellar Burgers». Закрывает драйвер по завершению теста.'''
     options = webdriver.ChromeOptions()
     options.add_argument(argument='--window-size=1920,1080')
     driver = webdriver.Chrome(options=options)
-    driver.get('https://stellarburgers.nomoreparties.site/')
-    return driver
+    driver.get(MAIN_PAGE)
+    yield driver
+    driver.quit()
 
 
 @pytest.fixture(scope='function')
-def log_in(driver, email, password):
-    '''Авторизация пользователя.'''
+def log_in(driver):
+    '''Аутентификация пользователя.'''
     driver.find_element(*loc.ACCOUNT_LINK).click()
-    driver.find_element(*loc.SIGN_IN_EMAIL_INPUT).send_keys(email)
-    driver.find_element(*loc.SIGN_IN_PASSWORD_INPUT).send_keys(password)
+    driver.find_element(*loc.SIGN_IN_EMAIL_INPUT).send_keys(EMAIL)
+    driver.find_element(*loc.SIGN_IN_PASSWORD_INPUT).send_keys(PASSWORD)
     driver.find_element(*loc.SIGN_IN_BUTTON).click()
     WebDriverWait(driver, 5).until(
         expected_conditions.element_to_be_clickable(loc.BASKET_BUTTON)
--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,51 +1,44 @@
+'''Тесты для проверки функциональности перехода между категориями
+в конструкторе web-сервиса «Stellar Burgers».'''
+from data import ACTIVE
 from tests.locators import Locators as loc
 
 
 class TestConstructor:
-    '''Тесты для проверки функциональности перехода между категориями
-    в конструкторе web-сервиса «Stellar Burgers».'''
-    def test_switch_to_buns_in_constructor(self, driver, active):
+
+    def test_switch_to_buns_in_constructor(sel, driver):
         '''Переход к категории «Булки» в конструкторе.'''
-        try:
-            driver.find_element(*loc.SAUCES).click()
-            buns_elem = driver.find_element(*loc.BUNS)
-            buns_elem.click()
-            toppings_elem = driver.find_element(*loc.TOPPINGS)
-            sauces_elem = driver.find_element(*loc.SAUCES)
-            assert (
-                active in buns_elem.get_attribute(name='class') and
-                active not in toppings_elem.get_attribute(name='class') and
-                active not in sauces_elem.get_attribute(name='class')
-            )
-        finally:
-            driver.quit()
+        driver.find_element(*loc.SAUCES).click()
+        buns_elem = driver.find_element(*loc.BUNS)
+        buns_elem.click()
+        toppings_elem = driver.find_element(*loc.TOPPINGS)
+        sauces_elem = driver.find_element(*loc.SAUCES)
+        assert (
+            ACTIVE in buns_elem.get_attribute(name='class') and
+            ACTIVE not in toppings_elem.get_attribute(name='class') and
+            ACTIVE not in sauces_elem.get_attribute(name='class')
+        )
 
-    def test_switch_to_sauces_in_constructor(self, driver, active):
+    def test_switch_to_sauces_in_constructor(self, driver):
         '''Переход к категории «Соусы» в конструкторе.'''
-        try:
-            sauces_elem = driver.find_element(*loc.SAUCES)
-            sauces_elem.click()
-            buns_elem = driver.find_element(*loc.BUNS)
-            toppings_elem = driver.find_element(*loc.TOPPINGS)
-            assert (
-                active in sauces_elem.get_attribute(name='class') and
-                active not in buns_elem.get_attribute(name='class') and
-                active not in toppings_elem.get_attribute(name='class')
-            )
-        finally:
-            driver.quit()
+        sauces_elem = driver.find_element(*loc.SAUCES)
+        sauces_elem.click()
+        buns_elem = driver.find_element(*loc.BUNS)
+        toppings_elem = driver.find_element(*loc.TOPPINGS)
+        assert (
+            ACTIVE in sauces_elem.get_attribute(name='class') and
+            ACTIVE not in buns_elem.get_attribute(name='class') and
+            ACTIVE not in toppings_elem.get_attribute(name='class')
+        )
 
-    def test_switch_to_toppings_in_constructor(self, driver, active):
+    def test_switch_to_toppings_in_constructor(self, driver):
         '''Переход к категории «Начинки» в конструкторе.'''
-        try:
-            toppings_elem = driver.find_element(*loc.TOPPINGS)
-            toppings_elem.click()
-            buns_elem = driver.find_element(*loc.BUNS)
-            sauces_elem = driver.find_element(*loc.SAUCES)
-            assert (
-                active in toppings_elem.get_attribute(name='class') and
-                active not in buns_elem.get_attribute(name='class') and
-                active not in sauces_elem.get_attribute(name='class')
-            )
-        finally:
-            driver.quit()
+        toppings_elem = driver.find_element(*loc.TOPPINGS)
+        toppings_elem.click()
+        buns_elem = driver.find_element(*loc.BUNS)
+        sauces_elem = driver.find_element(*loc.SAUCES)
+        assert (
+            ACTIVE in toppings_elem.get_attribute(name='class') and
+            ACTIVE not in buns_elem.get_attribute(name='class') and
+            ACTIVE not in sauces_elem.get_attribute(name='class')
+        )
